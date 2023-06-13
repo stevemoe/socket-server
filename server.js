fa83571e2d827024425a1623c83bd866b585dab6
Stefan <mohrst84031@th-nuernberg.de>
2023-06-13 04:18:02 +0200

    Merge remote-tracking branch 'origin/main'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http,{
     cors: {
-        origin: "http://https://socksonbots.de"
+        origin: "https://socksonbots.de"
     }
 });
 // const path = require("path");
