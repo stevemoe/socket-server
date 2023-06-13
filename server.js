ad090274169f09d17de0879ef16442db537e7c0a
Stefan <mohrst84031@th-nuernberg.de>
2023-06-13 02:38:22 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http,{
+    cors: {
+        origin: "https://socksonbots.de"
+    }
+});
+const path = require("path");
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server running</h1>");
+});
+
+io.on("connection", function (socket) {
+
+    console.log("A user with ID: " + socket.id + " connected");
+
+    socket.on("disconnect", function () {
+        console.log("A user with ID: " + socket.id + " disconnected");
+    });
+
+    socket.on("foo", (data) => {
+        socket.broadcast.emit("foo", data);
+        console.log("foo method");
+        console.log(data);
+    });
+
+    socket.on("direction", (data) => {
+        socket.broadcast.emit("direction", data);
+        console.log("direction method");
+        console.log(data);
+    });
+
+    socket.on("playerX", (data) => {
+        // socket.emit("direction", data);
+        console.log("playerX method");
+        console.log(data);
+    });
+
+});
+
+http.listen(3010, () => {
+    console.log("Listening on port *: 3010");
+});
